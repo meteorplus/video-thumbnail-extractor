979dfd60413033363c2e5578e99addebd8ec468b
Jonathan Ong <jonathanrichardong@gmail.com>
2015-04-05 01:01:00 -0700

    add caching via koa-s3-cache

--- config.js
+++ config.js
@@ -1,2 +1,8 @@
+'use strict'
 
-exports.password = process.env.PASSWORD || 'mgmt';
+exports.password = process.env.PASSWORD || 'mgmt'
+
+// s3 credentials
+exports.key = process.env.EXTRACTOR_AWS_ACCESS_KEY_ID || process.env.AWS_ACCESS_KEY_ID
+exports.secret = process.env.EXTRACTOR_SECRET_ACCESS_KEY || process.env.AWS_SECRET_ACCESS_KEY
+exports.bucket = process.env.EXTRACTOR_BUCKET || process.env.BUCKET
